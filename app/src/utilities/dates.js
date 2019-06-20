f786df0a4952eae62de4716c4682c3105164e01a
christopher wales <chriswales95@outlook.com>
2019-06-20 23:26:29 +0100

    fix up date stuff

--- app/src/utilities/dates.js
+++ app/src/utilities/dates.js
@@ -1,16 +1,44 @@
 module.exports = {
   format: function(date) {
-    var fullDate = new Date(date);
-    return (
-      fullDate.getFullYear() +
+    var date = new Date(date);
+    var hours = date.getHours();
+    var mins = date.getMinutes();
+    var seconds = date.getSeconds();
+    var month = date.getMonth();
+    var day = date.getDate();
+
+    if (seconds < 10) {
+      seconds = "0" + date.getSeconds();
+    }
+
+    if (mins < 10) {
+      mins = "0" + date.getMinutes();
+    }
+
+    if (hours < 10) {
+      hours = "0" + date.getHours();
+    }
+
+    if (month < 10) {
+      month = "0" + date.getMonth();
+    }
+
+    if (day < 10) {
+      day = "0" + date.getDate();
+    }
+
+    var currentDateandTime =
+      day +
       "/" +
-      fullDate.getMonth() +
+      month +
       "/" +
-      fullDate.getDate() +
+      date.getFullYear() +
       " " +
-      fullDate.getHours() +
+      hours +
       ":" +
-      fullDate.getMinutes()
-    );
+      mins +
+      ":" +
+      seconds;
+    return currentDateandTime;
   }
 };
